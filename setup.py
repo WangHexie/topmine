737cf270fd3612dd8255accf8d2edc1a5c09b112
wanghexie <31768052+WangHexie@users.noreply.github.com>
2021-08-06 15:43:46 +0800

    [fix] able to import now

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 import setuptools
 setup(
     name='topmine',
-    version='0.1.0',
+    version='0.1.1',
     description='topic mining',
     url='https://github.com/WangHexie/topmine',
     author='UNKNOWN',
     author_email='test@test.com',
     license='BSD 2-clause',
-    package_dir={"": "topmine"},
-    packages=setuptools.find_packages(where="topmine"),
+    # package_dir={"": "topmine"},
+    packages=["topmine"],
     install_requires=[
                       'numpy',
                       ],
